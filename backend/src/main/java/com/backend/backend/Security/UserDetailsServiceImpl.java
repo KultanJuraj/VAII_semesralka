b7762a318f0ef78ec4969f919ad64c9d4e2f0d20
KultanJuraj <kultanj1@gmail.com>
2025-12-08 00:12:52 +0100

    checkpoint_almost_done

---
+++ backend/src/main/java/com/backend/backend/Security/UserDetailsServiceImpl.java
@@ -0,0 +1,4 @@
+package com.backend.backend.Security;
+
+public class UserDetailsServiceImpl {
+}
--- backend/src/main/java/com/backend/backend/User/LoginRepository.java
+++
@@ -1,6 +0,0 @@
-package com.backend.backend.User;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LoginRepository extends JpaRepository<Login, Integer> {
-}
