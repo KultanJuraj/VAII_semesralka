1242c343f2d3e976265537bb120ee0d7b7de8f32
KultanJuraj <kultanj1@gmail.com>
2026-01-17 14:53:46 +0100

    Card versions on backend

---
+++ backend/src/main/java/com/backend/backend/Card/CardVersion.java
@@ -0,0 +1,15 @@
+package com.backend.backend.Card;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "card_versions")
+public class CardVesrsion {
+
+
+    @Id
+
+
+}
